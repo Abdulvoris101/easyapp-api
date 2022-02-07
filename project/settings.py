112adf5a546952ce67bdbb3296ce9ee7cd964ee9
Abdulvoris101 <erkinovabdulvoris101@gmail.com>
2022-02-07 21:58:26 +0500

    Heroku apply

--- project/settings.py
+++ project/settings.py
@@ -115,7 +115,6 @@ USE_TZ = True
 STATIC_ROOT = BASE_DIR / 'static'
 STATIC_URL = 'static/'
 
-django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
